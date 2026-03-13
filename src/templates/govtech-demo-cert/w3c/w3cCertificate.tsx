152ff3387d77f8a540d4e83c3a83bf07fceaf100
manishdex25 <manish.singh@dextech.ai>
2026-03-13 08:04:18 +0530

    fix: reverted

--- src/templates/govtech-demo-cert/w3c/w3cCertificate.tsx
+++ src/templates/govtech-demo-cert/w3c/w3cCertificate.tsx
@@ -119,7 +119,6 @@ const Page = styled("div")<{ certificateBg: string }>`
 
 export const CertificateTemplate: FunctionComponent<TemplateProps<GovtechW3cVerifiableCredential>> = ({ document }) => {
   const subject = document.credentialSubject;
-  const course = get(subject, "recipient.course");
   const issuedOn = get(subject, "issuedOn");
   const issuedOnDate = issuedOn ? new Date(issuedOn) : null;
 
@@ -140,7 +139,7 @@ export const CertificateTemplate: FunctionComponent<TemplateProps<GovtechW3cVeri
         <div className="spacer text-md">
           <i>has successfully completed the</i>
         </div>
-        <div className="spacer text-lg">{course}</div>
+        <div className="spacer text-lg">OpenCerts Demo</div>
       </section>
       <section>
         <div className="row align-items-center">
